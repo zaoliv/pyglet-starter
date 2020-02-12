0c9ecc8be14dc421e01a4817e048a14404ecf576
Jon <jstapleton463@harrisonburg.k12.va.us>
2020-02-12 11:18:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pyglet # import the library
 win = pyglet.window.Window() # create the window
 
-# Create some text
-# label = pyglet.text.Label('Hello, world', x = 200, y = 200)
-
-# Create a sprite
+# Load the image
 img = pyglet.image.load('assets/hero/Old hero.png')
+
 # spr = pyglet.sprite.Sprite(img, x=50, y=50)
 
 # Start the event loop
@@ -16,7 +14,6 @@ def update(dt):
 def on_draw():
     win.clear()
     img.blit(200, 100)
-    #img.blit(400, 100)
     # spr.draw()
 
 pyglet.clock.schedule(update) 
